b69cfefe9d49781ed19a121f1a633a0f800ad1b6
Ted Cai <ted.xmdgj@gmail.com>
2018-08-20 14:45:11 +0800

    fix test

--- test/controllers/chapters_controller_test.rb
+++ test/controllers/chapters_controller_test.rb
@@ -10,12 +10,12 @@ class ChaptersControllerTest < ActionDispatch::IntegrationTest
   
   test 'should be successful to open chapter list page' do
     get '/chapters'
-    assert :success
+    assert_response :success
   end
 
   test 'should be successful to open new chapter page' do
     get '/chapters/new'
-    assert :success
+    assert_response :success
   end
 
   test 'should be successful to create a new chapter' do
@@ -25,12 +25,12 @@ class ChaptersControllerTest < ActionDispatch::IntegrationTest
   
   test 'should be successful to open show page of a chapter' do
     get "/chapters/#{@chapter_one.id}"
-    assert :success
+    assert_response :success
   end
   
   test 'should be successful to open edit page of a chapter' do
     get "/chapters/#{@chapter_one.id}/edit"
-    assert :success
+    assert_response :success
   end
 
   test 'should be failed to update a chapter' do
@@ -42,31 +42,28 @@ class ChaptersControllerTest < ActionDispatch::IntegrationTest
   test 'should be successful to update a chapter' do
     put "/chapters/#{@chapter_one.id}", params: {chapter: {number: '1', active: '0'}}
     @chapter_one.reload
-    assert :success
     assert_equal false, @chapter_one.active
     assert_redirected_to @chapter_one
 
     put "/chapters/#{@chapter_two.id}", params: {chapter: {number: '2', active: '1'}}
     @chapter_two.reload
-    assert :success
     assert_equal true, @chapter_two.active
     assert_redirected_to @chapter_two
   end
   
   test 'should be successful to destroy a chapter' do
     delete "/chapters/#{@chapter_one.id}"
-    assert :success
     assert_redirected_to chapters_url
   end
   
   test 'test maze' do
     get '/maze/room/1'
-    assert :success
+    assert_response :success
     assert_equal @room_one, assigns(:room)
     assert_equal [@room_two], assigns(:available_rooms)
     
     get "/maze/room/#{@room_two.id}?chapter_id=#{@chapter_one.id}"
-    assert :success
+    assert_response :success
     assert_equal @room_two, assigns(:room)
     assert_empty assigns(:available_rooms)
   end
